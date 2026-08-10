86338a9ac67b6cf0d9660edb7cfe7287c3b846fa
DuongDangTu <161097655+DuongDangTu@users.noreply.github.com>
2026-08-10 15:25:11 +0700

    Add files via upload

---
+++ Qltv_lg/SqlServerTypes/infoStatic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qltv_lg.SqlServerTypes
+{
+    
+    class infoStatic
+    {
+        public static string txtUser = "";
+        public static int Quyen_ = 0  ;
+    }
+}
