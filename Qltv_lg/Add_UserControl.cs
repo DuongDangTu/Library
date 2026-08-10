7700b0d619c79fa0263e48c914209314e903f3e8
DuongDangTu <161097655+DuongDangTu@users.noreply.github.com>
2026-08-10 14:49:13 +0700

    Add files via upload

---
+++ Qltv_lg/Add_UserControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qltv_lg
+{
+    class Add_UserControl
+    {
+        public static void Add_UserControls(System.Windows.Forms.Control control , System.Windows.Forms.Control Pn_Main )
+        {
+            Pn_Main.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            control.BringToFront();
+            control.Focus();
+            Pn_Main.Controls.Add(control);
+
+        }
+    }
+}
---
+++ Qltv_lg/Program.cs
@@ -0,0 +1,23 @@
+﻿using Qltv_lg.Sach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qltv_lg
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Fm_Login());
+        }
+    }
+}
